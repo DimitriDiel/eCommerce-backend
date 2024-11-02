fd000577460e61c523069955ba2c17069dd036de
Dimitri Diel <150340219+DimitriDiel@users.noreply.github.com>
2024-11-02 11:51:41 +0400

    add product service

---
+++ services/product/src/main/java/org/ecommerce/product/Product.java
@@ -0,0 +1,27 @@
+package org.ecommerce.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.ecommerce.category.Category;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
